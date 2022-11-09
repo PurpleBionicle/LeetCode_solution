bd6f1c234dafa72e2ccc0279193b10b2ed6f0cf0
88888812 <ndp2012@yandex.ru>
2022-11-09 19:31:16 +0300

    add SQL solution

---
+++ SQL/Recyclable_and_Low_Fat_Products.py
@@ -0,0 +1,10 @@
+"""Recyclable and Low Fat Products"""
+#Write an SQL query to find the ids of products that are both low
+# fat and recyclable.
+#Return the result table in any order.
+#The query result format is in the following example.
+
+
+# Write your MySQL query statement below
+SELECT product_id FROM Products
+WHERE low_fats= 'Y' and recyclable = 'Y'
